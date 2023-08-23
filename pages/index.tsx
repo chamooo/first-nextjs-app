bb33925a8ebc701dbf9751928fcb2d601e3ddbed
Roman Melnychuk <roman4iek02@gmail.com>
2023-08-23 21:44:43 +0300

    try-catch

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,24 @@
 import Heading from '../components/Heading';
 
-
 export const getStaticProps = async () => {
-    const response = await fetch(`${process.env.API_HOST}/socials/`);
-    const data = await response.json();
+    try {
+        const response = await fetch(`${process.env.API_HOST}/socials/`);
+        const data = await response.json();
 
-    if (!data) return { notFound: true };
+        if (!data) return { notFound: true };
 
-    return {
-        props: { socials: data },
-    };
+        return {
+            props: { socials: data },
+        };
+    } catch {
+        return {
+            props: { socials: null },
+        };
+    }
 };
 
-
 const Home = ({ socials }) => {
-    return (
-        <Heading tag="h1" text="Hello"/>
-    );
+    return <Heading tag="h1" text="Hello" />;
 };
 
 export default Home;
